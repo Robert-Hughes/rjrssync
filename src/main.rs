244d342dba19574871d277ac490ecf697e497943
Rob Hughes <xman256@gmail.com>
2022-10-29 11:06:43 +0100

    Start work on setup_comms

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::net::{TcpListener, TcpStream};
+
 use clap::Parser;
-use log::{info};
+use log::{info, error};
 
 #[derive(clap::Parser)]
 struct CliArgs {
@@ -32,13 +34,30 @@ fn main() {
     let (src_host, src_folder) = parse_remote_folder(&args.src);
     let (dest_host, dest_folder) = parse_remote_folder(&args.dest);
 
-    // Launch rjrssync (if not already running) on remote hosts and estabilish communication
+    // Launch rjrssync (if not already running) on both remote hosts and estabilish communication
     let src_comms = setup_comms(&src_host);
+    if src_comms.is_none() {
+        return;
+    }
+    let src_comms = src_comms.unwrap();
+
     let dest_comms = setup_comms(&dest_host);
+    if dest_comms.is_none() {
+        return;
+    }
+    let dest_comms = dest_comms.unwrap();
 
     //TODO: tell the src to initiate the command, and it will contact the dest to request/send stuff
 
-    //TODO: Start command-processing loop, listening on a port
+    // Start command-processing loop, listening on a port for other instances to connect to us and make requests.
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    info!("Waiting for incoming connections...");
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        info!("Incoming connection from {:?}", stream);
+    }
+
 
     //TODO: if get a message telling us to start a transfer, setup_comms with the dest.
     //TODO:     get a list of all the files in the local dir, and ask the dest to do the same
@@ -46,12 +65,24 @@ fn main() {
     //TODO:     send over any files that have changed
 
     //TODO: if get a message telling us to provide a file list, do so
-    
+
     //TODO: if get a message telling us to receive a file, do so
 }
 
-fn setup_comms(remote_host: &str) {
-    info!("setup_comms with {}", remote_host);
+fn setup_comms(remote_host: &str) -> Option::<TcpStream> {
+    let remote_addr = remote_host.to_string() + ":7878";
+
+    info!("setup_comms with '{}'", remote_addr);
+
+    if let Ok(stream) = TcpStream::connect(&remote_addr) {
+        info!("Connected to '{}'", &remote_addr);
+        return Some(stream);
+    } else {
+        error!("Couldn't connect to '{}'", &remote_addr);
+        return None;
+    }
+    
+
     //TODO: attempt to connect to an already running instance, to save time
     //TODO: check the version is correct
     //TODO: if not, sync sources and run cargo build/run?
