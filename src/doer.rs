7f045b7c024a84f3def5a78131154eec3048d2b1
Joshua Slater <josh.miniac@gmail.com>
2022-11-04 14:37:22 +0000

    Rename primary and secondary and add colors in logging!
    Primary -> Boss
    Secondary -> Doer

--- src/doer.rs
+++ src/doer.rs
@@ -7,9 +7,9 @@ use walkdir::WalkDir;
 use crate::*;
 
 #[derive(clap::Parser)]
-struct SecondaryCliArgs {
+struct DoerCliArgs {
     #[arg(short, long)]
-    secondary: bool,
+    doer: bool,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -21,13 +21,13 @@ pub enum Command {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub enum Response {
-    // Split into lots of individual messages (rather than one big file list) so that the primary can start doing stuff before receiving the full list
-    File(String), 
+    // Split into lots of individual messages (rather than one big file list) so that the boss can start doing stuff before receiving the full list
+    File(String),
     Error(String)
 }
 
 enum Comms {
-    Local { // Local uses mpsc to communicate with the primary thread
+    Local { // Local uses mpsc to communicate with the boss thread
         sender: Sender<Response>,
         receiver: Receiver<Command>,
     },
@@ -68,7 +68,7 @@ impl Comms {
     }
 }
 impl Display for Comms {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { 
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Comms::Local { .. } => {
                 write!(f, "Local")
@@ -80,39 +80,39 @@ impl Display for Comms {
     }
 }
 
-pub fn secondary_main() -> ExitCode {
-    // Configure logging. Note that we can't use stdout as that is our communication channel with the primary!
+pub fn doer_main() -> ExitCode {
+    // Configure logging. Note that we can't use stdout as that is our communication channel with the boss!
     // We use stderr instead.
     //TODO: who is reading stderr? :O Log to file instead?
    // stderrlog::StdErrLog::new().init().unwrap();
 
-    info!("Running as secondary");
+    info!("Running as doer");
 
-    let _args = SecondaryCliArgs::parse();
+    let _args = DoerCliArgs::parse();
 
     // We take commands from our stdin and send responses on our stdout. These will be piped over ssh
-    // back to the Primary.
+    // back to the Boss.
 
-    // The first thing we send is a special message that the Primary will recognise, to know that we've started up correctly
+    // The first thing we send is a special message that the Boss will recognise, to know that we've started up correctly
     // and to make sure we are running compatible versions etc.
     // We need to do this on both stdout and stderr, because both those streams need to be synchronised on the receiving end.
     let msg = format!("{}{}", SECONDARY_HANDSHAKE_MSG, VERSION);
     println!("{}", msg);
     eprintln!("{}", msg);
 
-    // If the Primary isn't happy, they will stop us and deploy a new version. So at this point we can assume
+    // If the Boss isn't happy, they will stop us and deploy a new version. So at this point we can assume
     // they are happy and move on to processing commands they (might) send us
 
-    // Message-processing loop, until Primary disconnects.
+    // Message-processing loop, until Boss disconnects.
     message_loop(Comms::Remote);
 
-   // info!("Primary disconnected!");
+   // info!("Boss disconnected!");
 
     return ExitCode::from(22);
 }
 
-pub fn secondary_thread_running_on_primary(receiver: Receiver<Command>, sender: Sender<Response>) {
-    // Message-processing loop, until Primary disconnects.
+pub fn doer_thread_running_on_boss(receiver: Receiver<Command>, sender: Sender<Response>) {
+    // Message-processing loop, until Boss disconnects.
     message_loop(Comms::Local { sender, receiver });
 }
 
@@ -156,7 +156,7 @@ fn exec_command(command : Command, comms: &Comms) {
  //     {
 //         let start = Instant::now();
 //         let walker = WalkDir::new(&args.path).into_iter();
-//         let mut hash_sum: u8 = 0;        
+//         let mut hash_sum: u8 = 0;
 //         let mut count = 0;
 //         for entry in walker.filter_entry(|e| e.file_name() != ".git" && e.file_name() != "dependencies") {
 //             let e = entry.unwrap();
@@ -178,7 +178,7 @@ fn exec_command(command : Command, comms: &Comms) {
 
 //    let mut buf: [u8; 1] = [0];
 //    while stdin().read(&mut buf).unwrap_or(0) > 0 {
-      //  info!("Received data from Primary: {}", buf[0]);
+      //  info!("Received data from Boss: {}", buf[0]);
 
         // echo back
 //        stdout().write(&buf).unwrap();
