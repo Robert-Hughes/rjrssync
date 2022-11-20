94b362f05ee0a5e4a6f6057b03645bd9a79c76b5
Rob Hughes <xman256@gmail.com>
2022-11-20 22:36:39 +0000

    Fix and add some usage tests

--- tests/usage_tests.rs
+++ tests/usage_tests.rs
@@ -1,4 +1,4 @@
-use std::{path::Path, time::SystemTime};
+use std::{path::Path, time::{SystemTime, Duration}};
 
 use tempdir::TempDir;
 
@@ -20,7 +20,10 @@ impl FilesystemNode {
         FilesystemNode::Folder{name: name.to_string(), children: children.into() }
     }
     fn file(name: &str, contents: &str) -> FilesystemNode {
-        FilesystemNode::File{name: name.to_string(), contents: contents.as_bytes().to_vec() }       
+        FilesystemNode::File{name: name.to_string(), contents: contents.as_bytes().to_vec(), modified: SystemTime::now() }       
+    }
+    fn file_with_modified(name: &str, contents: &str, modified: SystemTime) -> FilesystemNode {
+        FilesystemNode::File{name: name.to_string(), contents: contents.as_bytes().to_vec(), modified }       
     }
 }
 
@@ -28,8 +31,9 @@ impl FilesystemNode {
 fn save_filesystem_tree_to_disk(tree: &[FilesystemNode], folder: &Path) { 
     for n in tree {
         match n {
-            FilesystemNode::File { name, contents } => {
+            FilesystemNode::File { name, contents, modified } => {
                 std::fs::write(folder.join(name), contents).unwrap();
+                filetime::set_file_mtime(folder.join(name), filetime::FileTime::from_system_time(*modified)).unwrap();
             },
             FilesystemNode::Folder { name, children } => {
                 std::fs::create_dir(folder.join(name)).unwrap();
@@ -48,6 +52,7 @@ fn load_filesystem_tree_from_disk(folder: &Path) -> Vec<FilesystemNode> {
             result.push(FilesystemNode::File {
                 name: entry.file_name().to_string_lossy().to_string(),
                 contents: std::fs::read(entry.path()).unwrap(),
+                modified: entry.metadata().unwrap().modified().unwrap()
             });
         } else if entry.file_type().unwrap().is_dir() {
             result.push(FilesystemNode::Folder {
@@ -64,7 +69,9 @@ fn load_filesystem_tree_from_disk(folder: &Path) -> Vec<FilesystemNode> {
 
 /// Checks that running rjrssync with src and dest folders containing the specified files/folders
 /// is successful and the dest folder ends up looking identical to the src folder.
-fn run_usage_test(src_tree: &[FilesystemNode], dest_tree: &[FilesystemNode]) {
+/// Optionally also tests that the expected number of files were copied.
+fn run_usage_test(src_tree: &[FilesystemNode], dest_tree: &[FilesystemNode], 
+    expected_num_copies: Option<u32>) {
     let src_dir = TempDir::new("rjrssync-test").unwrap();
     save_filesystem_tree_to_disk(src_tree, &src_dir.path());
 
@@ -72,10 +79,17 @@ fn run_usage_test(src_tree: &[FilesystemNode], dest_tree: &[FilesystemNode]) {
     save_filesystem_tree_to_disk(dest_tree, &dest_dir.path());
 
     let rjrssync_path = env!("CARGO_BIN_EXE_rjrssync");
-    std::process::Command::new(rjrssync_path)
+    let output = std::process::Command::new(rjrssync_path)
         .arg(src_dir.path())
         .arg(dest_dir.path())
-        .status().expect("rjrssync failed");
+        .output().expect("rjrssync failed");
+    assert!(output.status.success());
+
+    if let Some(expected_num_copies) = expected_num_copies {
+        let search = format!("Copied {} file(s)", expected_num_copies);
+        let actual = String::from_utf8(output.stderr).unwrap(); //TODO: not stdout?
+        assert!(actual.contains(&search));
+    }
 
     let new_dest_tree = load_filesystem_tree_from_disk(&dest_dir.path());
 
@@ -92,7 +106,7 @@ fn test_simple_sync() {
             FilesystemNode::file("sc", "contents3"),
         ])
     ];
-    run_usage_test(src_tree, &[]);
+    run_usage_test(src_tree, &[], Some(3));
 }
 
 /// Some files and a folder in the destination need deleting.
@@ -112,17 +126,34 @@ fn test_remove_dest_stuff() {
             FilesystemNode::file("sc", "contents3"),
         ])
     ];
-    run_usage_test(src_tree, dest_tree);
+    run_usage_test(src_tree, dest_tree, Some(3));
 }
 
 /// A file exists but has an old timestamp so needs updating.
 #[test]
 fn test_update_file() {
     let src_tree = &[
-        FilesystemNode::file("file", "contents1"),
+        FilesystemNode::file_with_modified("file", "contents1", SystemTime::UNIX_EPOCH + Duration::from_secs(1)),
     ];
     let dest_tree = &[
-        FilesystemNode::file("file", "contents2"),
+        FilesystemNode::file_with_modified("file", "contents2", SystemTime::UNIX_EPOCH),
     ];
-    run_usage_test(src_tree, dest_tree);
+    run_usage_test(src_tree, dest_tree, Some(1));
+}
+
+/// Most files have the same timestamp so don't need updating, but one does.
+#[test]
+fn test_skip_unchanged() {
+    let src_tree = &[
+        FilesystemNode::file_with_modified("file1", "contentsNEW", SystemTime::UNIX_EPOCH + Duration::from_secs(1)),
+        FilesystemNode::file_with_modified("file2", "contents2", SystemTime::UNIX_EPOCH),
+        FilesystemNode::file_with_modified("file3", "contents3", SystemTime::UNIX_EPOCH),
+    ];
+    let dest_tree = &[
+        FilesystemNode::file_with_modified("file1", "contentsOLD", SystemTime::UNIX_EPOCH),
+        FilesystemNode::file_with_modified("file2", "contents2", SystemTime::UNIX_EPOCH),
+        FilesystemNode::file_with_modified("file3", "contents3", SystemTime::UNIX_EPOCH),
+    ];
+    // Check that exactly one file was copied (the other two should have been skipped)
+    run_usage_test(src_tree, dest_tree, Some(1)); 
 }
