aaf9b1a1c62cbb6dce192e368a27c888cc5a2d24
Joshua Slater <josh.miniac@gmail.com>
2022-11-30 10:20:38 +0000

    Improve encrypted comms performance.
    Combine the cipher length and the cipher into one tcp write.

--- src/encrypted_comms.rs
+++ src/encrypted_comms.rs
@@ -1,6 +1,7 @@
 use std::{net::TcpStream, io::{Write, Read}};
 
-use aes_gcm::{Aes128Gcm, aead::{Nonce, Aead}};
+use bytes::{BytesMut, BufMut};
+use aes_gcm::{Aes128Gcm, aead::{Nonce, Aead}, AeadInPlace};
 use serde::{Deserialize, Serialize};
 
 use crate::profile_this;
@@ -9,10 +10,20 @@ pub fn send<T>(x: T, tcp_connection: &mut TcpStream, cipher: &Aes128Gcm,
     sending_nonce_counter: &mut u64, nonce_lsb: u64) -> Result<(), String>
     where T : Serialize,
 {
-    let unencrypted_data = {
+    profile_this!();
+    // Put the cipher and the length into a single buffer so we only do 1 tcp write.
+    // Allocate the size of the cipher + 8 bytes (for the cipher length at the start)
+    // Serialize into expands the buffer so with_capabity is used to reserve the buffer
+    // Allocate 1000 bytes extra to try and minimize allocations
+    let mut buffer = BytesMut::with_capacity((bincode::serialized_size(&x).unwrap() + 1008) as usize);
+    buffer.put_u64_le(0);
+    let mut cipher_len = BytesMut::split_to(&mut buffer, 8);
+    let mut writer = buffer.writer();
+    {
         profile_this!("Serialize");
-        bincode::serialize(&x).map_err(|e| "Error serializing command: ".to_string() + &e.to_string())?
-    };
+        bincode::serialize_into(&mut writer, &x).map_err(|e| "Error serializing command: ".to_string() + &e.to_string())?;
+    }
+    let mut buffer = writer.into_inner();
 
     // Nonces for boss -> doer should always be even, and odd for vice versa. They can't be reused between them.
     assert!(*sending_nonce_counter % 2 == nonce_lsb);
@@ -20,16 +31,16 @@ pub fn send<T>(x: T, tcp_connection: &mut TcpStream, cipher: &Aes128Gcm,
     nonce_bytes.resize(12, 0); // pad it
     let nonce = Nonce::<Aes128Gcm>::from_slice(&nonce_bytes);
     sending_nonce_counter.checked_add(2).unwrap(); // Increment by two so that it never overlaps with the nonce used by the doer
-
-    let ciphertext = {
+    {
         profile_this!("Encrypt");
-        cipher.encrypt(nonce, unencrypted_data.as_ref()).unwrap()
-    };
+        cipher.encrypt_in_place(nonce, &[], &mut buffer).unwrap();
+    }
+    cipher_len.copy_from_slice(&buffer.len().to_le_bytes());
+    cipher_len.unsplit(buffer);
 
     {
-        profile_this!("Tcp_Write");
-        tcp_connection.write_all(&ciphertext.len().to_le_bytes()).map_err(|e| "Error sending length: ".to_string() + &e.to_string())?;
-        tcp_connection.write_all(&ciphertext).map_err(|e| "Error sending length: ".to_string() + &e.to_string())?;
+        profile_this!("Tcp Write");
+        tcp_connection.write_all(&cipher_len).map_err(|e| "Error sending length: ".to_string() + &e.to_string())?;
 
         // Flush to make sure that we don't deadlock (other side waiting for data that never comes)
         tcp_connection.flush().map_err(|e| "Error flushing: ".to_string() + &e.to_string())?;
