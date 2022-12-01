ce4986c1e409c5e3e95b3030c397178f22b91390
Joshua Slater <josh.miniac@gmail.com>
2022-12-01 00:10:55 +0000

    Fix profiling module path

--- src/profiling/mod.rs
+++ src/profiling/mod.rs
@@ -1,14 +1,10 @@
 
 #[cfg(feature = "profiling")]
 pub mod profiling_real;
+#[cfg(feature = "profiling")]
+pub use profiling_real::*;
 
 #[cfg(not(feature = "profiling"))]
 pub mod profiling_fake;
-
-pub mod profiling{
-    #[cfg(feature = "profiling")]
-    pub use super::profiling_real::*;
-
-    #[cfg(not(feature = "profiling"))]
-    pub use super::profiling_fake::*;
-}
+#[cfg(not(feature = "profiling"))]
+pub use profiling_fake::*;
