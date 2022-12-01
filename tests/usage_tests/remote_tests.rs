5c04f5885a67a8d7a7418f9aa35f55cf15df5f9a
Rob Hughes <robert.hughes@arm.com>
2022-12-01 10:04:14 +0000

    Use env vars to configure remote test hosts

--- tests/usage_tests/remote_tests.rs
+++ tests/usage_tests/remote_tests.rs
@@ -2,23 +2,16 @@ use regex::Regex;
 
 use crate::test_framework::{run, TestDesc, empty_folder};
 
-/// The tests in this file rely on accessing "remote" hosts to test 
+/// The tests in this file rely on accessing "remote" hosts to test
 /// remote deploying and syncing. Therefore they require the test environment
-/// to be set up (e.g. firewalls configured, remote hosts configured), and 
+/// to be set up (e.g. firewalls configured, remote hosts configured), and
 /// a Windows and Linux remote hostname are required.
 /// One way of achieving this is to use WSL.
 
-//TODO: make these configurable, e.g. via env var?
-const REMOTE_USER_AND_HOST_WINDOWS: &str = "192.168.12.125";
-const REMOTE_TEMP_FOLDER_WINDOWS: &str = "T:\\Temp\\rjrssync-tests";
-
-const REMOTE_USER_AND_HOST_LINUX: &str = "rob@127.0.0.1";
-const REMOTE_TEMP_FOLDER_LINUX: &str = "/tmp/rjrssync-tests";
-
 /// Tests that rjrssync can be launched on a remote platform, and communication is estabilished.
-/// There is no proper sync performed (just syncing an empty folder), but this checks that 
+/// There is no proper sync performed (just syncing an empty folder), but this checks that
 /// the ssh/scp/cargo commands and TCP connection works.
-fn test_remote_launch_impl(remote_user_and_host: &str, remote_temp_folder: &str) {
+fn test_remote_launch_impl(remote_user_and_host: &str, remote_test_folder: &str) {
     // First run with --force-redeploy, to check that the remote deploying and building works,
     // even when the remote already has rjrssync set up.
     run(TestDesc {
@@ -28,7 +21,7 @@ fn test_remote_launch_impl(remote_user_and_host: &str, remote_temp_folder: &str)
         args: vec![
             "--force-redeploy".to_string(),
             "$TEMP/src".to_string(),
-            format!("{remote_user_and_host}:{remote_temp_folder}")
+            format!("{remote_user_and_host}:{remote_test_folder}")
         ],
         expected_exit_code: 0,
         expected_output_messages: vec![
@@ -37,14 +30,14 @@ fn test_remote_launch_impl(remote_user_and_host: &str, remote_temp_folder: &str)
         ..Default::default()
     });
 
-    // Then run without --force-redeploy, and it should use the existing copy 
+    // Then run without --force-redeploy, and it should use the existing copy
     run(TestDesc {
         setup_filesystem_nodes: vec![
             ("$TEMP/src", &empty_folder())
         ],
         args: vec![
             "$TEMP/src".to_string(),
-            format!("{remote_user_and_host}:{remote_temp_folder}")
+            format!("{remote_user_and_host}:{remote_test_folder}")
         ],
         expected_exit_code: 0,
         unexpected_output_messages: vec![
@@ -56,10 +49,18 @@ fn test_remote_launch_impl(remote_user_and_host: &str, remote_temp_folder: &str)
 
 #[test]
 fn test_remote_launch_windows() {
-    test_remote_launch_impl(REMOTE_USER_AND_HOST_WINDOWS, REMOTE_TEMP_FOLDER_WINDOWS);
+    let user_and_host = std::env::var("RJRSSYNC_TEST_REMOTE_USER_AND_HOST_WINDOWS")
+        .expect("Missing env var required for test environment: RJRSSYNC_TEST_REMOTE_USER_AND_HOST_WINDOWS");
+    let test_folder = std::env::var("RJRSSYNC_TEST_REMOTE_TEST_FOLDER_WINDOWS")
+        .expect("Missing env var required for test environment: RJRSSYNC_TEST_REMOTE_TEST_FOLDER_WINDOWS");
+    test_remote_launch_impl(&user_and_host, &test_folder);
 }
 
 #[test]
 fn test_remote_launch_linux() {
-    test_remote_launch_impl(REMOTE_USER_AND_HOST_LINUX, REMOTE_TEMP_FOLDER_LINUX);
+    let user_and_host = std::env::var("RJRSSYNC_TEST_REMOTE_USER_AND_HOST_LINUX")
+        .expect("Missing env var required for test environment: RJRSSYNC_TEST_REMOTE_USER_AND_HOST_LINUX");
+    let test_folder = std::env::var("RJRSSYNC_TEST_REMOTE_TEST_FOLDER_LINUX")
+        .expect("Missing env var required for test environment: RJRSSYNC_TEST_REMOTE_TEST_FOLDER_LINUX");
+    test_remote_launch_impl(&user_and_host, &test_folder);
 }
