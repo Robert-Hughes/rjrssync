6bcf33458190b31da3b53ffb9a4a47de9cb16b8a
Rob Hughes <xman256@gmail.com>
2023-01-02 15:45:10 +0000

    Update benchmark graph

--- .github/workflows/generate-benchmark-graphs.py
+++ .github/workflows/generate-benchmark-graphs.py
@@ -16,7 +16,7 @@ def main():
     args = parser.parse_args()
 
     #e.g. benchmark-results-wsl-2023-01-02-13-26-127ca5f54c8605f9f872548a1157f2b595981863.json
-    filename_regex = re.compile(".*(\d+)-(\d+)-(\d+)-(\d+)-(\d+)-([0-9a-zA-Z]+).json")    
+    filename_regex = re.compile(".*?(\d+)-(\d+)-(\d+)-(\d+)-(\d+)-([0-9a-zA-Z]+).json")    
     all_results = pandas.DataFrame()
     for dirpath, dirs, files in os.walk(args.json_files):
         for filename in files:
@@ -62,8 +62,14 @@ def main():
     times = all_results.loc[all_results["measurement"] == "time"]
     fig = px.scatter(times, x="timestamp", y="value", facet_row="case", facet_col="target",
         color="program",
-        hover_data=["commit-hash"])
+        hover_data=["commit-hash"],
+        symbol_sequence=["x"])
     fig.update_yaxes(matches=None)
+   # fig.layout.yaxis.matches ='y'
+  #  fig.layout.yaxis2.matches ='y'
+ #   fig.layout.yaxis3.matches ='y'
+    fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
+    print(f"Writing html to {args.output_html}...")
     fig.write_html(args.output_html)
 
 
