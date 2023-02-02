139d94702e161007871f04e69f5f4758de9d44b5
Rob Hughes <xman256@gmail.com>
2023-02-02 18:35:18 +0000

    Generate second page of graphs with evenly spaced timestamps

--- .github/workflows/generate-benchmark-graphs.py
+++ .github/workflows/generate-benchmark-graphs.py
@@ -11,7 +11,7 @@ from datetime import datetime
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--json-files', required=True) 
+    parser.add_argument('--json-files', required=True)
     parser.add_argument('--output-html', required=True)
     args = parser.parse_args()
 
@@ -57,24 +57,39 @@ def main():
                                     "value": value,
                                 }
                                 all_results = pandas.concat([all_results, pandas.DataFrame.from_records([row])])
-    
+
+
+    all_timestamps = all_results["timestamp"].unique()
+    all_timestamps.sort()
+    all_timestamps = all_timestamps.tolist()
+    timestamp_to_order = { t:all_timestamps.index(t) for t in all_timestamps }
+    all_results["order"] = all_results["timestamp"].astype('int64').apply(lambda t: timestamp_to_order[t])
+
     print(all_results)
 
-    # fig = px.scatter(all_results, x="timestamp", y="value", facet_row="case", facet_col="measurement",
-    #     color="program",
-    #     hover_data=["commit-hash"])
     times = all_results.loc[all_results["measurement"] == "time"]
+
+
+
     fig = px.scatter(times, x="timestamp", y="value", facet_row="case", facet_col="target",
         color="program",
-        hover_data=["commit-hash"],
+        hover_data=["commit-hash", "timestamp", "order"],
         symbol_sequence=["x"])
     fig.update_yaxes(matches=None)
-   # fig.layout.yaxis.matches ='y'
-  #  fig.layout.yaxis2.matches ='y'
- #   fig.layout.yaxis3.matches ='y'
     fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
     print(f"Writing html to {args.output_html}...")
     fig.write_html(args.output_html)
+
+
+    fig = px.scatter(times, x="order", y="value", facet_row="case", facet_col="target",
+        color="program",
+        hover_data=["commit-hash", "timestamp", "order"],
+        symbol_sequence=["x"])
+    fig.update_yaxes(matches=None)
+    fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
+    second_output_html = args.output_html.replace(".html", "-order.html")
+    print(f"Writing html to {second_output_html}...")
+    fig.write_html(second_output_html)
 
 
 if __name__ == "__main__":
